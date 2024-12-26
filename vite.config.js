eb12f03f3017e0f0121ef93c941acc6ec340b7dc
hunter-nguyen <hunternguyen8@gmail.com>
2024-12-26 19:59:21 +0700

    update route deployment with 404.html

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.VITE_LAST_UPDATED': JSON.stringify(new Date().toLocaleString()),
+  base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Optional: prevents splitting into multiple files
+      },
+    },
   },
-  base: "/hunter-nguyen.github.io/"
-})
+});
